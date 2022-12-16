bbe2085247408767c3c52a6db6b17174680ea34a
Angal11 <agalih9870@gmail.com>
2022-12-16 15:37:24 +0700

    Learn 6 add profile

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
@@ -6,3 +7,17 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title','featured_image' , 'description', 
                   'demo_link', 'source_link', 'tags' ]
+        widgets = {
+            'tags':forms.CheckboxSelectMultiple(),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name , field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+
+        # self.fields['title'].widget.attrs.update({'class':'input', 'placeholder': 'Add title'})
+        # self.fields['description'].widget.attrs.update({'class':'input', 'placeholder': 'Add title'})
+        # self.fields['demo_link'].widget.attrs.update({'class':'input', 'placeholder': 'Add title'})
+        # self.fields['source_link'].widget.attrs.update({'class':'input', 'placeholder': 'Add title'})
